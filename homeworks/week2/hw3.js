6082eb6b59b71c6f5c3474a0ae40ddb4ff04bd42
Bryan <48666047+a9874687@users.noreply.github.com>
2020-07-18 14:42:01 +0800

    week2完成

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  
+  var reverseStr = ''
+  for(var i = str.length; i >= 0; i--) {
+    reverseStr += str[i]
+  }
+  console.log(reverseStr)
 }
 
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,20 @@
 function join(arr, concatStr) {
-  
+    if(arr.length === 0) {
+        return ''
+    }
+    var answer = arr[0]
+    for(var i = 1; i < arr.length;  i++) {
+        answer += concatStr + arr[i]
+    }
+    return answer
 }
 
 function repeat(str, times) {
-  
+    var answer = ''
+    for(var i = 1; i <= times; i++) {
+        answer += str
+    }
+    return answer
 }
 
 console.log(join(['a'], '!'));

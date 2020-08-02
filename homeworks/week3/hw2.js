b6f4dbe2d0ce6c566ea4430fc49c1eb325629893
Bryan <48666047+a9874687@users.noreply.github.com>
2020-08-02 21:05:30 +0800

    week3完成

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -2,7 +2,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable no-plusplus */
-/* eslint-disable no-const-assign */
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-const */
 const readline = require('readline');
 
 const lines = [];
@@ -20,9 +21,9 @@ rl.on('close', () => {
 });
 
 function solve(input) {
-  const temp = input[0].split(' ');
-  const n = Number(temp[0]);
-  const m = Number(temp[1]);
+  let temp = input[0].split(' ');
+  let n = Number(temp[0]);
+  let m = Number(temp[1]);
   for (let i = n; i <= m; i += 1) {
     if (isNarcissistic(i)) {
       console.log(i);
@@ -36,18 +37,18 @@ numCount = (n) => {
     return 1;
   }
   while (n !== 0) {
-    m = Math.floor(n / 10);
+    n = Math.floor(n / 10);
     result++;
   }
   return result;
 };
 
 isNarcissistic = (n) => {
-  const m = n;
-  const total = numCount(n);
+  let m = n;
+  let total = numCount(n);
   let sum = 0;
   while (m !== 0) {
-    const num = m % 10;
+    let num = m % 10;
     sum += num ** total;
     m = Math.floor(m / 10);
   }
